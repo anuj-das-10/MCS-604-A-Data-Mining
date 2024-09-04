8444222d0cd7681559bf79dcd8cb76d6879f784b
Anuj Das <anonboy1434@gmail.com>
2024-09-05 02:29:06 +0530

    TRIE DS

---
+++ OP.java
@@ -0,0 +1,16 @@
+class OP {
+    public static void main(String []args) {
+        
+        // 10 x (2 ^ 2)
+        System.out.println(10 << 2);
+
+        // 10 x (2 ^ 3)
+        System.out.println(10 << 3);
+
+        // 32 / (2 ^ 2)
+        System.out.println(32 >> 2);
+
+        // 32 / (2 ^ 3)
+        System.out.println(32 >> 3);
+    }
+}
